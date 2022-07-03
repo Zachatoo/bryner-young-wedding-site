603e42a7f72ddf100e425f18d98d69092621baab
Zachatoo <Zachatoo@users.noreply.github.com>
2022-07-03 10:34:55 -0600

    Update yarn, fix eslint

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,14 +6,16 @@ const weddingDate = new Date("2022-09-09");
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
         <title>Bryner-Young Wedding</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-8 text-center">
-        <h1 className="text-6xl font-bold font-great-vibes">We're Engaged!</h1>
+      <main className="flex flex-col items-center justify-center flex-1 w-full px-8 text-center">
+        <h1 className="text-6xl font-bold font-great-vibes">
+          {"We're Engaged!"}
+        </h1>
         <Countdown targetTime={weddingDate} className="py-8" />
       </main>
     </div>
