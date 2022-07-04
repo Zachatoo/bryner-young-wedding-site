3e6dbcf05fa84dee82df9b01d9d4a7f3e69c50e6
Zachatoo <Zachatoo@users.noreply.github.com>
2022-07-03 18:08:10 -0600

    Homepage styling updates Add Montserrat font. Add theme colors. More responsive design.

--- components/countdown/DateTimeDisplay.tsx
+++ components/countdown/DateTimeDisplay.tsx
@@ -5,7 +5,7 @@ interface Props {
 
 export function DateTimeDisplay({ value, type }: Props) {
   return (
-    <div className="flex flex-col text-lg">
+    <div className="flex flex-col sm:text-lg text-md">
       <span data-testid={`countdown-${type}`}>{value}</span>
       <span>{type}</span>
     </div>
