e76975470c78f334bf5f77d3fb2edf21ac34a044
Zachatoo <Zachatoo@users.noreply.github.com>
2022-07-10 23:09:08 -0600

    Add missing export for Alert from components folder

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,7 @@
 export * from "./countdown";
 export * from "./form";
 export * from "./layout";
+export * from "./Alert";
 export * from "./Button";
 export * from "./Head";
 export * from "./Icon";
