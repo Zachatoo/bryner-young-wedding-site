8d3c3b95a52113bfa56d9e75e95e30ba4264c10e
Zachatoo <Zachatoo@users.noreply.github.com>
2022-07-17 21:32:42 -0600

    Add banner image Add temporary banner image to home page until engagement pictures are ready.
    #18

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import { Head } from "components";
 import dynamic from "next/dynamic";
 import { LOCAL_STORAGE_KEYS, useLocalStorage } from "hooks";
+import Image from "next/image";
 
 const Countdown = dynamic(() => import("../components/countdown/Countdown"), {
   ssr: false,
@@ -22,25 +23,37 @@ const HomePage: NextPage = () => {
   ).format(weddingDate);
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <>
       <Head></Head>
 
-      <main className="flex flex-col items-center justify-center flex-1 w-full px-8 text-center">
-        <div className="flex flex-col pb-8 text-center sm:pb-16 font-great-vibes">
+      <main className="flex flex-col items-center justify-center flex-1 w-full px-8 pb-8 text-center sm:pb-16">
+        <div className="relative banner w-screen h-[300px] sm:h-[700px] pb-4 sm:pb-8">
+          <Image
+            src="/images/home-banner.jpg"
+            alt="Zach and MK on temple grounds"
+            layout="fill"
+            objectFit="cover"
+            priority
+          />
+          <h1 className="hidden sm:block z-10 mt-auto pb-0 text-6.5xl sm:pb-8 text-white sm:text-8xl font-great-vibes capitalize">
+            {rsvpStatus ? "See you at the wedding!" : "We're getting married!"}
+          </h1>
+        </div>
+        <h1 className="block sm:hidden mt-auto pb-6 text-6.5xl sm:pb-8 sm:text-8xl font-great-vibes capitalize">
+          {rsvpStatus ? "See you at the wedding!" : "We're getting married!"}
+        </h1>
+        <div className="flex flex-col pb-8 text-center sm:pb-12 font-great-vibes">
           <span className="text-3xl sm:text-5xl">Mary Katherine Bryner</span>
           <span className="text-xl sm:text-2xl">&</span>
           <span className="text-3xl sm:text-5xl">Zachary Matthew Young</span>
         </div>
-        <h1 className="pb-6 text-6.5xl sm:pb-16 sm:text-8xl font-great-vibes capitalize">
-          {rsvpStatus ? "See you at the wedding!" : "We're getting married!"}
-        </h1>
         <div className="pb-4 text-3xl sm:text-5xl font-great-vibes">
           {formattedWeddingDate}
         </div>
         <Countdown targetTime={weddingDate} className="pb-6 sm:pb-8" />
         <span>More details to come...</span>
       </main>
-    </div>
+    </>
   );
 };
 
