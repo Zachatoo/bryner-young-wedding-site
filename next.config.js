2ffcdf123c1dc0d1b74fb7b7892762f81167ce2a
Zachatoo <Zachatoo@users.noreply.github.com>
2022-09-09 11:35:33 -0600

    Add zoom link closes #32

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  redirects: async () => {
+    return [
+      {
+        source: "/live",
+        destination: "https://zoom.us",
+        permanent: false,
+      },
+    ];
+  },
+};
